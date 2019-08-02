d9e382c622a2a5fa188060642455fafd62268523
Kainã Almeida <kainafa@hotmail.com>
2019-08-02 17:34:38 -0300

    up project

---
+++ lib/blocs/home.bloc.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/widgets.dart';
+import 'package:shopping_bloc/models/category-list-item.model.dart';
+import 'package:shopping_bloc/models/product-list-item.model.dart';
+import 'package:shopping_bloc/repositories/category.repository.dart';
+import 'package:shopping_bloc/repositories/product.repository.dart';
+
+class HomeBloc extends ChangeNotifier {
+  final categoryRepository = new CategoryRepository();
+  final productRepository = new ProductRepository();
+
+  List<ProductListItemModel> products;
+  List<CategoryListItemModel> categories;
+
+  String selectedCategory = 'todos';
+
+  HomeBloc() {
+    getCategories();
+    getProducts();
+  }
+
+  getCategories() {
+    categoryRepository.getAll().then((data) {
+      this.categories = data;
+      notifyListeners();
+    });
+  }
+
+  getProducts() {
+    productRepository.getAll().then((data) {
+      this.products = data;
+      notifyListeners();
+    });
+  }
+
+  getProductsByCategory() {
+    productRepository.getByCategory(selectedCategory).then((data) {
+      this.products = data;
+      notifyListeners();
+    });
+  }
+
+  changeCategory(tag) {
+    selectedCategory = tag;
+    products = null;
+    getProductsByCategory();
+    notifyListeners();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_bloc/blocs/home.bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_bloc/ui/shared/widgets/category/category-list.widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<HomeBloc>.value(
+          value: HomeBloc(),
+        )
+      ],
+      child: Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping Cart',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final HomeBloc bloc = Provider.of<HomeBloc>(context);
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              "Categorias",
+              style: Theme.of(context).textTheme.headline,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CategoryList(
+              categories: bloc.categories,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
